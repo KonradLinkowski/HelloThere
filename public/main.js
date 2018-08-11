df307c5c467b180877a40d178585a87f3f16f2f8
Konrad Linkowski <linkowski.konrad97@gmail.com>
2018-08-11 23:10:04 +0200

    Improved frontend part to work with backend.

--- public/main.js
+++ public/main.js
@@ -6,19 +6,54 @@
   const $sendBtn = document.querySelector('#chat-send')
   const $searchBtn = document.querySelector('#search')
 
-  $searchBtn.addEventListener('click', search)
-  $sendBtn.addEventListener('click', e => {
+  const socket = new Socket('/chat')
 
+  $searchBtn.addEventListener('click', login)
+  $sendBtn.addEventListener('click', e => {
+    socket.sendMessage($chatInput.value, msg => printMessage(msg, true))
   })
   $chatInput.addEventListener('keydown', e => {
-
+    if (e.key === 'Enter') {
+      socket.sendMessage($chatInput.value, msg => printMessage(msg, true))
+    }
   })
 
-  function search() {
+  socket.on('join', data => {
+    console.log('joined')
+  })
+
+  socket.on('message', msg => printMessage(msg, false))
+  socket.on('user-left', () => {
+    socket.leave()
+    $chatBox.innerHTML = ''
+    $loginPage.setActive(true)
+    $chatPage.setActive(false)
+  })
+
+  function printMessage(message, you) {
+    let mes = document.createElement('p')
+    mes.innerHTML = (you ? 'You: ' : 'Stranger: ') + message
+    $chatBox.appendChild(mes)
+  }
+
+  function login() {
     const $myGender = document.querySelector('#my-gender')
     const $searchGender = document.querySelector('#search-gender')
-    $loginPage.setActive(false)
-    $chatPage.setActive(true)
+    const myGender = document.querySelector('input[name=my-gender]:checked').dataset.gender
+    const checkboxes = document.querySelectorAll('input[name=search-gender]:checked')
+    const searchFor = []
+    for (let e of checkboxes) {
+      searchFor.push(e.dataset.gender)
+    }
+    socket.login(myGender, searchFor, succes => {
+      if (succes) {
+        socket.search()
+        $loginPage.setActive(false)
+        $chatPage.setActive(true)
+      } else {
+        console.log('Login failure')
+      }
+    })
   }
 })()
 
--- public/socket.js
+++ public/socket.js
@@ -1,5 +1,58 @@
-const socket = io.connect('http://localhost:3000')
-socket.on('news', data => {
-  console.log(data)
-  socket.emit('my other event', { my: 'data' })
-})
+class Socket {
+  /**
+   * Creates new socket
+   * 
+   * @param {string} namespace where to connect
+   */
+  constructor (namespace) {
+    this.socket = io.connect(namespace)
+  }
+
+  /**
+   * Logs in new user
+   * 
+   * @param {string} myGender male or female
+   * @param {string[]} searchFor array of male or female
+   * @param {function} cb what to do when server will log user in
+   */
+  login(myGender, searchFor, cb) {
+    this.socket.emit('login', {
+      myGender: myGender,
+      searchFor: searchFor
+    }, cb)
+  }
+
+  /**
+   * Leaves current conversation
+   */
+  leave() {
+    this.socket.emit('leave')
+  }
+
+  /**
+   * Sends message to a current connected user
+   * 
+   * @param {string} message your message
+   * @param {functiion} cb what to do when server process the message
+   */
+  sendMessage(message, cb) {
+    this.socket.emit('message', message, cb)
+  }
+
+  /**
+   * Starts searching for new user
+   */
+  search() {
+    this.socket.emit('search')
+  }
+
+  /**
+   * Overrides event's behaviour
+   * 
+   * @param {string} event name of the event
+   * @param {function} fn what to do
+   */
+  on(event, fn) {
+    this.socket.on(event, fn)
+  }
+}
