ac51ba580b5aa3164c1681aefec805967cb7be15
Konrad Linkowski <linkowski.konrad97@gmail.com>
2018-08-14 20:57:02 +0200

    Improved server code to be more readable.

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+'use strict'
 const User = require('./user')
 
 /**
@@ -12,79 +13,77 @@ const chatQueue = []
 
 setInterval(matchUsers, 100)
 
+let chat = null
+
 function serv(io) {
-  const chat = io.of('/chat')
-  chat.on('connection', socket => {
-    console.log(`${socket.id} connected`)
+  chat = io.of('/chat')
+  chat.on('connection', handleConnection)
+}
 
-    socket.on('login', (data, fn) => {
-      console.log(`${socket.id} logged in`)
-      if ((data.myGender === 'male' || data.myGender === 'female')
-        && data.searchFor.length >= 1 && data.searchFor.length <= 2) {
-        users.push(new User(socket, data.myGender, data.searchFor))
-        fn(true)
-      } else {
-        fn(false)
-      }
-    })
+function handleConnection(socket) {
+  console.log(`${socket.id} connected`)
 
-    socket.on('logout', () => {
-      console.log(`${socket.id} logged out`)
-      let thisUser = users.find(u => u.socket.id == socket.id)
-      if (!thisUser) return
-      users.splice(users.indexOf(thisUser))
-    })
+  socket.on('login', (data, fn) => {
+    console.log(`${socket.id} logged in`)
+    if ((data.myGender === 'male' || data.myGender === 'female')
+      && data.searchFor.length >= 1 && data.searchFor.length <= 2) {
+      users.push(new User(socket, data.myGender, data.searchFor))
+      fn(true)
+    } else {
+      fn(false)
+    }
+  })
 
-    socket.on('search', data => {
-      if (data === true) {
-        console.log(`${socket.id} is looking for someone`)
-        const thisUser = users.find(user => user.socket.id == socket.id)
-        chatQueue.push(thisUser)
-      } else {
-        console.log(`${socket.id} stopped looking for someone`)
-        const thisUser = users.find(user => user.socket.id == socket.id)
-        chatQueue.splice(chatQueue.indexOf(thisUser), 1)
-      }
-    })
+  socket.on('logout', () => {
+    console.log(`${socket.id} logged out`)
+    let thisUser = users.find(u => u.socket.id == socket.id)
+    if (!thisUser) return
+    users.splice(users.indexOf(thisUser))
+  })
 
-    socket.on('leave', data => {
-      console.log(`${socket.id} left the room`)
-      let room = Object.keys(socket.rooms)[1]
-      chat.to(room).emit('user-left')
-      socket.leave(room)
-    })
+  socket.on('search', data => {
+    if (data === true) {
+      console.log(`${socket.id} is looking for someone`)
+      const thisUser = users.find(user => user.socket.id == socket.id)
+      chatQueue.push(thisUser)
+    } else {
+      console.log(`${socket.id} stopped looking for someone`)
+      const thisUser = users.find(user => user.socket.id == socket.id)
+      chatQueue.splice(chatQueue.indexOf(thisUser), 1)
+    }
+  })
 
-    socket.on('message', (msg, fn) => {
-      console.log(`${socket.id} sent message: ${msg}`)
-      if (msg === '') {
-        return fn({ error: true })
-      }
-      let room = Object.keys(socket.rooms)[1]
-      socket.to(room).emit('message', { error: false, msg: msg })
-      fn({ error: false, msg: msg })
-    })
+  socket.on('leave', data => {
+    console.log(`${socket.id} left the room`)
+    let room = Object.keys(socket.rooms)[1]
+    chat.to(room).emit('user-left')
+    socket.leave(room)
+  })
 
-    socket.on('disconnecting', data => {
-      console.log(`${socket.id} is disconnecting`)
-      let room = Object.keys(socket.rooms)[1]
-      chat.to(room).emit('user-left')
-      // chat.in(room).clients((error, clients) => {
-      //   if (error) throw error;
-      //   for(let i = 0; i < clients.length; i++){
-      //     chat.sockets.connected[clients[i]].leave(room)
-      //   }
-      // });
-    })
+  socket.on('message', (msg, fn) => {
+    console.log(`${socket.id} sent message: ${msg}`)
+    if (msg === '') {
+      return fn({ error: true })
+    }
+    let room = Object.keys(socket.rooms)[1]
+    socket.to(room).emit('message', { error: false, msg: msg })
+    fn({ error: false, msg: msg })
+  })
 
-    socket.on('disconnect', data => {
-      console.log(`${socket.id} disconnected`)
-      let user = users.find(user => user.socket.id == socket.id)
-      if (!user) return
-      let indexOf = users.indexOf(user)
-      if (indexOf) users.splice(indexOf, 1)
-      indexOf = chatQueue.indexOf(user)
-      if (indexOf) chatQueue.splice(indexOf, 1)
-    })
+  socket.on('disconnecting', data => {
+    console.log(`${socket.id} is disconnecting`)
+    let room = Object.keys(socket.rooms)[1]
+    chat.to(room).emit('user-left')
+  })
+
+  socket.on('disconnect', data => {
+    console.log(`${socket.id} disconnected`)
+    let user = users.find(user => user.socket.id == socket.id)
+    if (!user) return
+    let indexOf = users.indexOf(user)
+    if (indexOf) users.splice(indexOf, 1)
+    indexOf = chatQueue.indexOf(user)
+    if (indexOf) chatQueue.splice(indexOf, 1)
   })
 }
 
