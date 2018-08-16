b33de861564d1282815f72d429824386ae2078a8
Konrad Linkowski <linkowski.konrad97@gmail.com>
2018-08-16 21:39:53 +0200

    Added mustache render engine and language system support.

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const express = require('express')
 const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
+const mustacheExpress = require('mustache-express')
+const locale = require('locale')
+const supported = ['en', 'pl']
+
+const lang = require('./lang/lang')
 
 const port = process.env.PORT || 3000
 
@@ -13,4 +18,22 @@ server.listen(port, () => {
   console.log(`Server port: ${port}`)
 })
 
+app.use(locale(supported))
+
+app.engine('mustache', mustacheExpress())
+
+app.set('view engine', 'mustache')
+app.set('views', path.join(__dirname, '/views'))
+
 app.use('/', express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+  res.render('main', {
+    userCount: 999,
+    whoAreYou: lang.whoAreYou[req.locale],
+    whoAreYouLookingFor: lang.whoAreYouLookingFor[req.locale],
+    female: lang.female[req.locale],
+    male: lang.male[req.locale],
+    saySomething: lang.saySomething[req.locale]
+  })
+})
